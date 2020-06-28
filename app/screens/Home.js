c5dc189475e857379a42b293a648a9d62c5070f9
kenny-tech <kennyemma2008@gmail.com>
2020-06-28 10:32:12 +0100

    Configured and set up navigation, added Home Screen to navigation

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Home = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Home Screen</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default Home;
