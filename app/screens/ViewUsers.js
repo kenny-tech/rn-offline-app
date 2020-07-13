3475d7388ae4aa26067dc57fad6341010e673acd
kenny-tech <kennyemma2008@gmail.com>
2020-07-13 18:13:20 +0100

    Fixed Register User and View Users bugs

---
+++ app/screens/ViewUsers.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect  } from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+import { DB } from '../model/db';
+
+const ViewUsers = () => {
+
+    const [flatListItems, setFlatListItems] = useState([]);
+
+    const listViewItemSeparator = () => {
+        return (
+            <View style={{ height: 0.2, width: '100%', backgroundColor: '#808080' }} />
+        )
+    }
+
+    const getUsers = () => {
+        DB.transaction(tx => {
+            tx.executeSql('SELECT * FROM allusers', [], (tx, results) => {
+                let temp = [];
+                console.log(results.rows);
+                for (let i = 0; i < results.rows.length; ++i) {
+                    temp.push(results.rows.item(i));
+                }
+                setFlatListItems(temp);
+            })
+        });
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, []);
+
+    return (
+        <View>
+            <FlatList
+                data={flatListItems}
+                ItemSeparatorComponent={listViewItemSeparator}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View style={{ backgroundColor: 'white', padding: 20 }}>
+                        <Text>Name: {item.name}</Text>
+                        <Text>Phone: {item.phone}</Text>
+                        <Text>Address: {item.address}</Text>
+                    </View>
+                )}
+            />
+        </View>
+    )
+    
+}
+
+export default ViewUsers;
