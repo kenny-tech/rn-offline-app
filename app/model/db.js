11a1fb7bc544226f9da0d2621c3a1e35f94a089e
kenny-tech <kennyemma2008@gmail.com>
2020-07-13 18:17:32 +0100

    Cleanup db

--- app/model/db.js
+++ app/model/db.js
@@ -5,25 +5,11 @@ const database_name = "usersdb.db";
 export const DB = SQLite.openDatabase({name: database_name , location: 'default'}, successcb, errorcb);
 
 function successcb() {
-  console.log('OK Databases changes')
+  console.log('Database Opened')
 }
 
 function errorcb() {
    console.log('error DB')
 }
-
-// import SQLite from 'react-native-sqlite-storage';
-
-// const database_name = "users.db";
-
-// export const DB = SQLite.openDatabase({name: database_name, location: 'default'}, openCB, errorCB);
-
-// function openCB() {
-//   console.log("Database Opened");
-// }
-
-// function errorCB(err) {
-//   console.log("SQL Error: " + err);
-// }
 
 
